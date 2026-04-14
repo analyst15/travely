fec76a1d5c7a004d0702e1b9c81db9bb83da0faf
analyst15 <alexokeyo15@gmail.com>
2026-04-14 21:40:48 +0300

    new able created

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,13 +1,23 @@
-import { pgTable, serial, text, integer, date } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, integer, date, boolean, timestamp, json, } from "drizzle-orm/pg-core";
 
 export const tours = pgTable("tours", {
   id: serial("id").primaryKey(),
   title: text("title").notNull(),
   destination: text("destination").notNull(),
   price: integer("price").notNull(),
-
   availableFrom: date("available_from"),
   availableTo: date("available_to"),
-
   maxGuests: integer("max_guests").notNull(), // 🔥 NEW
 });
+
+export const destinations = pgTable("destinations", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  slug: text("slug").unique().notNull(),
+  image: text("image").notNull(),
+  expeditions: integer("expeditions").default(0),
+  tags: json("tags").$type<string[]>(),
+  featured: boolean("featured").default(false),
+  createdAt: timestamp("created_at").defaultNow(),
+})
+
