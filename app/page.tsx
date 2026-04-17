014048fa185a9ab2409c7852ecf8f5e6918492e3
analyst15 <alexokeyo15@gmail.com>
2026-04-17 21:15:44 +0300

    added the sections to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Navbar from "@/components/navbar";
 import About from "@/components/sections/about";
+import CTASection from "@/components/sections/cta-section";
 import GetawayCTA from "@/components/sections/cta/getaway-cta";
 import { FeaturedDestinations } from "@/components/sections/featured-destinations";
 import Hero from "@/components/sections/hero";
+import Testimonials from "@/components/sections/testimonials";
 import FeaturedToursSection from "@/components/sections/tours/FeaturedToursSection";
 import WhyChooseUs from "@/components/sections/why-choose-us";
 import Image from "next/image";
@@ -17,6 +19,8 @@ export default function Home() {
       <FeaturedDestinations />
       <GetawayCTA />
       <FeaturedToursSection />
+      <Testimonials />
+      <CTASection />
     </main>
   );
 }
