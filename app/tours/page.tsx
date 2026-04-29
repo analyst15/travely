0d2a5b2439197802c218fe89171eb695d7015ab7
analyst15 <alexokeyo15@gmail.com>
2026-04-29 21:51:39 +0300

    update: tours

--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -1,3 +1,38 @@
+import Image from "next/image";
+
+
+export default function AboutPage() {
+  return (
+    <div className="w-full">
+                <section className="relative h-100 w-full">
+                    <Image
+                        src="/images/about-hero.jpg"
+                        alt="About Hero"
+                        fill
+                        className="object-cover"
+                        priority
+                    />
+    
+                    {/* Overlay */}
+                    <div className="absolute inset-0 bg-black/50" />
+    
+                    {/* Content */}
+                    <div className="relative z-10 flex h-full flex-col items-center justify-center text-center text-white px-4">
+                        <h1 className="text-4xl md:text-5xl font-bold">Tours</h1>
+                        <p className="mt-4 max-w-2xl text-sm md:text-base text-gray-200">
+                            Esse dolorum voluptatem ullam est sint nemo et est ipsa porro placeat quibusdam quia assumenda numquam molestias.
+                        </p>
+    
+                        <div className="mt-3 text-sm text-gray-300">
+                            Home <span className="mx-2">/</span>{" "}
+                            <span className="text-green-400">Destinations</span>
+                        </div>
+                    </div>
+                </section>
+            </div>
+  )
+}
+
 // import { db } from "../db";
 // import { tours } from "../db/schema";
 // import { and, eq, gte, lte } from "drizzle-orm";
